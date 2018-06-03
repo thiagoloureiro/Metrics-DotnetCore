5fa2634a561d39201a05f75da7f4afad1d84a4c1
Thiago Loureiro <thiagoguaru@me.com>
2018-06-03 21:55:26 +0200

    Project Rename

---
+++ Metrics-Test/Controllers/HelloController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Metrics_Test.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Hello")]
+    public class HelloController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Hello", "World" };
+        }
+    }
+}
